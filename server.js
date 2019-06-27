a23eef3057d11a99c5686eb0c150378f234238c5
deanban <pbp2005@gmail.com>
2019-06-26 22:28:34 -0400

    server.js edited

--- server.js
+++ server.js
@@ -35,3 +35,23 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 //passport middleware
 app.use(passport.initialize());
+
+/*********************SET UPS*************************/
+
+//server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+/*********************PORT*************************/
+const port = process.env.PORT || 3001;
+
+app.listen(port, () =>
+  console.log(`***********Server Running on Port ${port}***********`)
+);
+/*********************PORT*************************/
