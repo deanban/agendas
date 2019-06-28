58330e0b42eb5926ac375f08b03249e877b1a7ec
deanban <pbp2005@gmail.com>
2019-06-27 20:31:05 -0400

    task sql edited

--- bin/sql/task.sql
+++ bin/sql/task.sql
@@ -1,10 +1,14 @@
-CREATE TABLE project (
+CREATE TABLE task (
   id                              SERIAL PRIMARY KEY,
   body                            TEXT,
   about                           TEXT,
   done                            BOOLEAN NOT NULL DEFAULT 'f',
   "accountId"                     INTEGER,
+  "projectId"                     INTEGER,
+  "personalId"                    INTEGER,
   "createdAt"                     TIMESTAMPTZ DEFAULT Now(),
   "doneAt"                        TIME with time zone,
   FOREIGN KEY ("accountId")       REFERENCES account(id),
+  FOREIGN KEY ("projectId")       REFERENCES project(id),
+  FOREIGN KEY ("personalId")      REFERENCES personal(id),
 );
