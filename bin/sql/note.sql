eef8f1a3fe5b2b352a1d8dbbbfd1998b532f62bb
deanban <pbp2005@gmail.com>
2019-06-27 20:36:26 -0400

    note sql edited

---
+++ bin/sql/note.sql
@@ -0,0 +1,15 @@
+CREATE TABLE note (
+  id                              SERIAL PRIMARY KEY,
+  title                           TEXT,
+  body                            TEXT,
+  "accountId"                     INTEGER NOT NULL,
+  "projectId"                     INTEGER,
+  "personalId"                    INTEGER,
+  "taskId"                        INTEGER,
+  "createdAt"                     TIMESTAMPTZ DEFAULT Now(),
+  "doneAt"                        TIME with time zone,
+  FOREIGN KEY ("accountId")       REFERENCES account(id),
+  FOREIGN KEY ("projectId")       REFERENCES project(id),
+  FOREIGN KEY ("personalId")      REFERENCES personal(id),
+  FOREIGN KEY ("taskId")          REFERENCES task(id)
+);
